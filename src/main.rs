ee24fb682a144f635e1586fdd2d42361933f890a
bogosorter <luiswbarbosa@gmail.com>
2025-08-19 20:08:58 +0200

    tested image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use image::{RgbImage, Rgb};
+
 fn main() {
-    println!("hello, world");
+    let width = 1920;
+    let height = 1080;
+    let mut image = RgbImage::new(width, height);
+
+    for x in 0..width {
+        for y in 0.. height {
+            image.put_pixel(x, y, Rgb([(x * 255 / width) as u8, (y * 255 / height) as u8, 100]));
+        }
+    }
+
+    match image.save("output.png") {
+        Ok(()) => println!("Image saved"),
+        Err(e) => eprintln!("Couldn't save image: {}", e)
+    }
 }
