5d97988f87865b082902de30fdd9a4f526641266
bogosorter <luiswbarbosa@gmail.com>
2025-08-21 18:41:54 +0200

    added progress indicator

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,21 @@ mod geometry;
 mod raytracer;
 use raytracer::pixel_color;
 mod utils;
-use utils::to_rgb;
 
 fn main() {
     let mut image = RgbImage::new(WIDTH, HEIGHT);
 
     for x in 0..WIDTH {
+        utils::print_progress(x as f32 / WIDTH as f32);
+
         for y in 0.. HEIGHT {
-            image.put_pixel(x, HEIGHT - y - 1, to_rgb(&pixel_color(x, y)));
+            image.put_pixel(x, HEIGHT - y - 1, utils::to_rgb(&pixel_color(x, y)));
         }
     }
 
+    utils::print_progress(1.);
+    println!("");
+    
     match image.save("output.png") {
         Ok(()) => println!("Image saved"),
         Err(e) => eprintln!("Couldn't save image: {}", e)
--- src/utils.rs
+++ src/utils.rs
@@ -4,3 +4,9 @@ use crate::geometry::vector::Vector;
 pub fn to_rgb(vector: &Vector) -> Rgb<u8> {
     Rgb([(vector.x * 225.) as u8, (vector.y * 255.) as u8, (vector.z * 255.) as u8])
 }
+
+pub fn print_progress(progress: f32) {
+    let percentage = progress * 100.;
+    print!("\r{percentage:.1}%");
+    std::io::Write::flush(&mut std::io::stdout()).unwrap();
+}
